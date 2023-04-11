a7d4d1eea6d9e24c0bfa98455fe0635cd3dec795
Jerry Bennett <jerrbennett2@gmail.com>
2023-04-11 10:35:27 -0400

    Updated popup.html to look much nicer
    Also cleaned up various code problems.

--- tagVariables.js
+++ tagVariables.js
@@ -1,27 +1,22 @@
 function getTriggerVariables() {
     const triggerVariables = document.getElementsByClassName('gtm-predicate-summary-row');
-    const numOfTriggers = triggerVariables.length;
-  
     const variablesToStore = {
       filter1: [],
       filter2: [],
       filter3: [],
-      numOfTriggers,
+      numOfTriggers: triggerVariables.length,
       triggerName: ''
     };
   
     for (const triggerVariable of triggerVariables) {
-        //sleep(1000);
-  
       const triggerName = triggerVariable.closest('.gtm-tag-row').querySelector('.wd-open-trigger-button.fill-cell.md-gtm-theme').innerHTML;
-      const filter1 = triggerVariable.children[0].innerHTML;
-      const filter2 = triggerVariable.children[1].innerHTML;
-      const filter3 = triggerVariable.children[2].innerHTML;
+      let filter1 = triggerVariable.children[0].innerHTML;
+      let filter2 = triggerVariable.children[1].innerHTML;
+      let filter3 = triggerVariable.children[2].innerHTML;
   
       variablesToStore.filter1.push(filter1);
       variablesToStore.filter2.push(filter2);
       variablesToStore.filter3.push(filter3);
-  
       variablesToStore.triggerName = triggerName;
     }
   
