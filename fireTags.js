fc5a75bcb4da9861b2ce174414d09410b210d92f
Jerry Bennett <jerrbennett2@gmail.com>
2023-04-12 11:19:24 -0400

    Fixed nested storage calls

--- fireTags.js
+++ fireTags.js
@@ -1,61 +1,62 @@
 const fireTagsFunction = async () => {
   var element;
-  var elementLength
-  chrome.storage.sync.get(["numOfTriggers"]).then((result) => {
-    var numOfTriggers = result.numOfTriggers;
-    var previewClick1 = "";
+  var elementLength;
+  chrome.storage.sync.get(["numOfTriggers", "filter1", "filter2", "filter3"], (result) => {
+    const numOfTriggers = result.numOfTriggers;
+    const clickElements = [
+      result.filter1,
+      result.filter2,
+      result.filter3
+    ];
     const internalLinks = Array.from(document.links).filter(link => link.hostname === window.location.hostname);
     const numPages = internalLinks.length;
     console.log(`This website has ${numPages} pages.`);
+
+    var previewClick1 = "";
+    console.log(`This website has ${numPages} pages.`);
     for (let i = 0; i < numOfTriggers; i++) {
+      // Use the clickElements array instead of multiple chrome.storage.sync.get calls
+      const clickElement1 = clickElements[0];
+      const clickElement2 = clickElements[1];
+      const clickElement3 = clickElements[2];
+      
       //get variables from other script and set them here.
-      chrome.storage.sync.get(["filter1"]).then((result) => {
-        var clickElement1 = result.filter1;
+      //if statements for each option
+      if (String(clickElement1[i]).includes("Click")) {
+        //CLICK
+        if (clickElement1.includes("Classes")) {
+          previewClick1 = document.getElementsByClassName(String(clickElement3[i]));
+        } else if (String(clickElement1[i]).includes("Element")) {
+          previewClick1 = document.getElementsByName(String(clickElement3[i]));
+        }
+        //REST OF CLICK OPTIONS HERE
+        else if (String(clickElement1[i]).includes("Text")) {
+          previewClick1 = "Click Text";
+        }
+      }
+      if (String(clickElement2).includes("equals")) {
+        element = document.getElementsByClassName(clickElement3[i]);
+        elementLength = document.getElementsByClassName(clickElement3[i]).length;
 
-        chrome.storage.sync.get(["filter2"]).then((result) => {
-          var clickElement2 = result.filter2;
-
-          chrome.storage.sync.get(["filter3"]).then((result) => {
-            var clickElement3 = result.filter3;
-
-            //if statements for each option
-            if (String(clickElement1[i]).includes("Click")) {
-              //CLICK
-              if (clickElement1.includes("Classes")) {
-                previewClick1 = document.getElementsByClassName(String(clickElement3[i]));
-              } else if (String(clickElement1[i]).includes("Element")) {
-                previewClick1 = document.getElementsByName(String(clickElement3[i]));
-              }
-              //REST OF CLICK OPTIONS HERE
-              else if (String(clickElement1[i]).includes("Text")) {
-                previewClick1 = "Click Text"
-              }
-            }
-            if (String(clickElement2).includes("equals")) {
-              element = document.getElementsByClassName(clickElement3[i]);
-              elementLength = document.getElementsByClassName(clickElement3[i]).length;
-
-              if (elementLength > 0) {
-                for (let i = 0; i < elementLength; i++) {
-                  element[i].click();
-                }
-              }
-            }
-            if (String(clickElement2).includes("contains")) {
-              element = document.getElementsByClassName(clickElement3[i]);
-              elementLength = document.getElementsByClassName(clickElement3[i]).length;
-              if (elementLength > 0) {
-                for (let i = 0; i < elementLength; i++) {
-                  element[i].click();
-                }
-              }
-            }
-          });
-        });
-      });
+        if (elementLength > 0) {
+          for (let i = 0; i < elementLength; i++) {
+            element[i].click();
+          }
+        }
+      }
+      if (String(clickElement2).includes("contains")) {
+        element = document.getElementsByClassName(clickElement3[i]);
+        elementLength = document.getElementsByClassName(clickElement3[i]).length;
+        if (elementLength > 0) {
+          for (let i = 0; i < elementLength; i++) {
+            element[i].click();
+          }
+        }
+      }
     }
   });
 };
+
 
 const scrapeSite = async (callback) => {
   const internalLinks = Array.from(document.links).filter(link => link.hostname === window.location.hostname);
