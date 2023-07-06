f69bda88246db7b5645685f12b013e5e02878647
Jerry Bennett <jerrbennett2@gmail.com>
2023-07-06 12:47:46 -0400

    Update fireTags.js

--- fireTags.js
+++ fireTags.js
@@ -10,11 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
-
+  
   async function fireTagsFunction() {
   chrome.storage.sync.get({ currentOptionText: '', tagsArray: [], triggerArray: []}, function (data) {
     var currentOptionText = data.currentOptionText;
-    console.log('Current option: '+ currentOptionText);
     var tagsArray = data.tagsArray;
     var triggerArray = data.triggerArray
     var numOfTags = tagsArray[0].length;
@@ -26,32 +25,25 @@ document.addEventListener('DOMContentLoaded', () => {
         // Check for specific default variables using a case switch
         switch (triggerArray[1][i]) {
           case 'Click Classes':
-            // Find how many elements on the page match the 'Click Classes' value
-            var clickClasses = document.getElementsByClassName(tagsArray[1][i]);
-            // Click each one of those elements
-            for (var j = 0; j < clickClasses.length; j++) {
-              clickClasses[j].click();
-            }
-            break;
+          var clickClasses = document.getElementsByClassName(tagsArray[1][i]);
+          for (var j = 0; j < clickClasses.length; j++) {
+            clickClasses[j].click();
+          }
+          break;
 
-          case 'Click Element':
-            // Find how many elements on the page match the 'Click Element' value
-            var clickElements = document.querySelectorAll(tagsArray[1][i]);
-            // Click each one of those elements
-            for (var j = 0; j < clickClasses.length; j++) {
-              clickElements[j].click();
-            }
-            break;
+        case 'Click Element':
+          var clickElements = document.querySelectorAll(tagsArray[1][i]);
+          for (var j = 0; j < clickElements.length; j++) {
+            clickElements[j].click();
+            addSquare(clickElements[j]);
+          }
+          break;
 
-            case 'Click ID':
-              // Find how many elements on the page match the 'Click Element' value
-              var clickIDs = document.getElementById(tagsArray[1][i]);
-              for (var j = 0; j < clickClasses.length; j++) {
-                clickIDs[j].click();
-              }
-              break;
-              
-
+        case 'Click ID':
+          var clickIDs = document.getElementById(tagsArray[1][i]);
+          clickIDs.click();
+          addSquare(clickIDs);
+          break;
           default:
             // Handle other cases if necessary
             break;
@@ -60,5 +52,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 }
-
-
