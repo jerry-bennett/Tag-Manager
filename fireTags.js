701212bcd1b6ded1d8c4790dbad44a97105f7133
Jerry Bennett <jerrbennett2@gmail.com>
2023-07-17 11:08:49 -0400

    Updated QA button

--- fireTags.js
+++ fireTags.js
@@ -19,36 +19,56 @@ document.addEventListener('DOMContentLoaded', () => {
     var numOfTags = tagsArray[0].length;
 
     // Loop through each tag name and see if it matches the one in the dropdown
-    for (var i = 0; i < numOfTags; i++) {
+    for (let i = 0; i < numOfTags; i++) {
       // See if the current option is what is listed in the dropdown
       if (String(tagsArray[0][i]).includes(currentOptionText)) {
         // Check for specific default variables using a case switch
         switch (triggerArray[1][i]) {
           case 'Click Classes':
-          var clickClasses = document.getElementsByClassName(tagsArray[1][i]);
-          for (var j = 0; j < clickClasses.length; j++) {
-            clickClasses[j].classList.add("highlighted");
-            clickClasses[j].click();
-            // Add a CSS class to the clicked element
-          }
-          break;
+            clickElementsByClass(tagsArray[1][i]);
+            break;
 
-        case 'Click Element':
-          var clickElements = document.querySelectorAll(tagsArray[1][i]);
-          for (var j = 0; j < clickElements.length; j++) {
-            clickElements[j].click();
-          }
-          break;
+          case 'Click Element':
+            clickElementsBySelector(tagsArray[1][i]);
+            break;
 
-        case 'Click ID':
-          var clickIDs = document.getElementById(tagsArray[1][i]);
-          clickIDs.click();
-          break;
+          case 'Click ID':
+            clickElementById(tagsArray[1][i]);
+            break;
+
           default:
-            // Handle other cases if necessary
+            //temporary error handling
+            console.log("No elements matching that tag.")
             break;
         }
       }
+    }
+
+    function clickElementsByClass(className) {
+      const clickClasses = document.getElementsByClassName(className);
+      const numClickClasses = clickClasses.length;
+      for (let j = 0; j < numClickClasses; j++) {
+        const element = clickClasses[j];
+        //element.classList.add("highlighted");
+        element.style.border = "5px solid red";
+        console.log("Class list " + element.classList);
+        //element.click();
+        // Add a CSS class to the clicked element
+      }
+    }
+
+    function clickElementsBySelector(selector) {
+      const clickElements = document.querySelectorAll(selector);
+      const numClickElements = clickElements.length;
+      for (let j = 0; j < numClickElements; j++) {
+        const element = clickElements[j];
+        element.click();
+      }
+    }
+
+    function clickElementById(id) {
+      const clickElement = document.getElementById(id);
+      clickElement.click();
     }
   });
 }
--- qaTags.js
+++ qaTags.js
@@ -1,27 +1,76 @@
-//when the user has the GTM window open after firing the tags
+//wait until click before running any script
 document.addEventListener('DOMContentLoaded', () => {
-    const qaTags = document.getElementById("qaTags");
-  
+  const qaTags = document.getElementById("qaTags");
     qaTags.addEventListener("click", async () => {
       let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
+
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: qaTagsFunction,
+        func: qaTagsFunction
       });
     });
-  })
-  
-  //defining sleep
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+  });
   
   async function qaTagsFunction() {
-    chrome.storage.sync.get(["tagName"]).then((result) => {
-        var tagName = result.tagName;
-        var events = document.getElementsByClassName('message-list__title');
-        var firedTags = document.getElementsByClassName('tags-tab__fired-tags');
-        console.log(tagName);
-    })
+  chrome.storage.sync.get({ currentOptionText: '', tagsArray: [], triggerArray: []}, function (data) {
+    var currentOptionText = data.currentOptionText;
+    var tagsArray = data.tagsArray;
+    var triggerArray = data.triggerArray
+    var numOfTags = tagsArray[0].length;
+
+    // Loop through each tag name and see if it matches the one in the dropdown
+    for (let i = 0; i < numOfTags; i++) {
+      // See if the current option is what is listed in the dropdown
+      if (String(tagsArray[0][i]).includes(currentOptionText)) {
+        // Check for specific default variables using a case switch
+        switch (triggerArray[1][i]) {
+          case 'Click Classes':
+            highlightElementsByClass(tagsArray[1][i]);
+            break;
+
+          case 'Click Element':
+            highlightElementsBySelector(tagsArray[1][i]);
+            break;
+
+          case 'Click ID':
+            clickElementById(tagsArray[1][i]);
+            break;
+
+          default:
+            //temporary error handling
+            console.log("No elements matching that tag.")
+            break;
+        }
+      }
+    }
+
+    function highlightElementsByClass(className) {
+      const clickClasses = document.getElementsByClassName(className);
+      const numClickClasses = clickClasses.length;
+      for (let j = 0; j < numClickClasses; j++) {
+        const element = clickClasses[j];
+    
+        // Toggle the red rectangle by checking if the border style is already applied
+        if (element.style.border === "5px solid red") {
+          element.style.border = ""; // If border is present, remove it
+        } else {
+          element.style.border = "5px solid red"; // If border is not present, add it
+        }
+      }
+    }
+
+    function highlightElementsBySelector(selector) {
+      const clickElements = document.querySelectorAll(selector);
+      const numClickElements = clickElements.length;
+      for (let j = 0; j < numClickElements; j++) {
+        const element = clickElements[j];
+        element.click();
+      }
+    }
+
+    function clickElementById(id) {
+      const clickElement = document.getElementById(id);
+      clickElement.click();
+    }
+  });
 }
